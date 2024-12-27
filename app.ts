645ffb26ac5dff4f8b40cf3b96b320e07e6eb419
VicenteRV <juan-vizz@hotmail.com>
2024-12-26 18:46:42 -0600

    Servidor listo

--- app.ts
+++ app.ts
@@ -1,2 +1,10 @@
-export const nombre = 'vicente';
-console.log(nombre);
+import dotenv from "dotenv";
+import Server from "./models/server";
+//configuracion del dot.env para var entorno
+dotenv.config();
+
+const server = new Server();
+
+server.listen();
+
+
